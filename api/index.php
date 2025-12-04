e70c8fdb31abe27689c05999c082371105dbc7e8
AISYAH <h1101241044@student.untan.ac.id>
2025-12-04 16:38:37 +0700

    Up

--- api/index.php
+++ api/index.php
@@ -1,11 +1,5 @@
 <?php
-<<<<<<< HEAD
-// api/index.php
-=======
 // api/index.php - VERCEL ENTRY POINT
-// ISI HANYA INI SAJA:
-
->>>>>>> 436296297ae3bc4292313dd1b0b95eac90ba58de
 require_once _DIR_ . '/../public/index.php';
 ?>
 
