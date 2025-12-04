24d2c4f2a049329910e91be28db7bfd716abb071
Febrianti10 <fbrntkhmrh@gmail.com>
2025-12-04 16:42:31 +0700

    index api

--- api/index.php
+++ api/index.php
@@ -1,12 +1,4 @@
 <?php
-<<<<<<< HEAD
-// api/index.php
-=======
 // api/index.php - VERCEL ENTRY POINT
-// ISI HANYA INI SAJA:
-
->>>>>>> 436296297ae3bc4292313dd1b0b95eac90ba58de
 require_once _DIR_ . '/../public/index.php';
 ?>
-
-<!-- HAPUS SEMUA KODE LAIN, PASTIKAN TIDAK ADA WHITESPACE SETELAH ?> -->
