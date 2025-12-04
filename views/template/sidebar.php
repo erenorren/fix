1f48ba7efcc6d16f290857b2c48ef6fca8614df1
kharizma <h1101241050@student.untan.ac.id>
2025-12-04 09:18:32 +0700

    up

--- views/template/sidebar.php
+++ views/template/sidebar.php
@@ -15,7 +15,7 @@
   <!-- Brand -->
   <div class="sidebar-brand d-flex align-items-center">
     <div class="brand-logo-circle me-2">
-      <img src="<?= $base_url ?>/img/LOGO.png" class="brand-logo-img" alt="Logo">
+      <img src="<?= $base_url ?>/img/logo.png" class="brand-logo-img" alt="Logo">
     </div>
     <div class="d-flex flex-column">
       <span class="brand-name text-white fw-bold">SIP Hewan</span>
